ee426cbc47c5730d011900545eb4093fdc3fe518
3lv <cristian.vlad.vladutu@gmail.com>
2025-02-16 17:45:04 +0200

    Fixed url for readme*

--- custom_components/SM16inpind/data.py
+++ custom_components/SM16inpind/data.py
@@ -1,5 +1,5 @@
 FULL_NAME = "Sixteen INPUTS"
-LINK = "https://sequentmicrosystems.com/products/16-universal-inputs-card-for-raspberry-pi"
+LINK = "https://sequentmicrosystems.com/products/sixteen-lv-digital-inputs-card-for-raspberry-pi"
 
 import lib16inpind
 API = lib16inpind
